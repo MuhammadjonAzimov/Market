84da9cfa6b7b8344d4874d39a4f536cf36c7247b
MuhammadjonAzimov <muhammadazimov921@gmail.com>
2022-01-31 12:27:56 +0500

    style berilldi

--- product.js
+++ product.js
@@ -1,23 +1,93 @@
 const productsData = [
     {
+        imgurl: "./images/non.jpg",
         id: 1,
         title: "Non",
         price: 3000
     },
     {
+        imgurl: "./images/cola1.jpg",
         id: 2,
         title: "Cola",
         price: 10000
     },
     {
+        imgurl: "./images/olma.jpg",
         id: 3,
         title: "Olma",
-        price: 15000
+        price: 11500
     },
     {
+        imgurl: "./images/uzum.jpg",
         id: 4,
         title: "Uzum",
         price: 7000
+    },
+    {
+        imgurl: "./images/tuhum.jpg",
+        id: 5,
+        title: "Tuhum",
+        price: 13000
+    },
+    {
+        imgurl: "./images/мука.jpg",
+        id: 6,
+        title: "Un",
+        price: 11400
+    },
+    {
+        imgurl: "./images/banan.jpg",
+        id: 7,
+        title: "Banan",
+        price: 15000
+    },
+    {
+        imgurl: "./images/sok.jpg",
+        id: 8,
+        title: "Sok",
+        price: 7900
+    },
+    {
+        imgurl: "./images/makaron.jpg",
+        id: 9,
+        title: "Makaron",
+        price: 9990
+    },
+    {
+        imgurl: "./images/chips.jpg",
+        id: 10,
+        title: "Chips",
+        price: 6490
+    },
+    {
+        imgurl: "./images/yog.jpg",
+        id: 11,
+        title: "Yog'",
+        price: 42000
+    },
+    {
+        imgurl: "./images/saryog.jpg",
+        id: 12,
+        title: "Saryog'",
+        price: 18490
+    },
+    {
+        imgurl: "./images/kolbasa.jpg",
+        id: 13,
+        title: "Kolbasa",
+        price: 49490
+    },
+    {
+        imgurl: "./images/steyk.jpg",
+        id: 14,
+        title: "Steyk",
+        price: 109990
+    },
+    {
+        imgurl: "./images/sut.jpg",
+        id: 15,
+        title: "Sut",
+        price: 11490
     }
 ]
 
@@ -29,12 +99,13 @@ function renderProducts(products) {
         div.classList.add("product")
 
         div.innerHTML = `
+            <img class="product-img" src="${product.imgurl}" alt="">
             <h3>${product.title}</h3>
-            <B>${product.price}</b>
+            <B>${product.price} so'm</b>
             <button onclick="addProductToCart(${product.id})" id="add-to-cart" data-product-id="${product.id}">Savatchaga qo'shish</button>
         `
 
-        productsContainer.appendChild(div)
+        productsContainer.append(div)
     }
 }
 
